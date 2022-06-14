a6b29a82feed6305a61956ef1888b0fd8ddc48cc
李煜超 <yuchao.li@pingcap.com>
2022-06-14 16:23:16 +0800

    Issue fix: value of items are not rendered at expected way.

--- website/src/components/issues/renderer/Affection.js
+++ website/src/components/issues/renderer/Affection.js
@@ -16,11 +16,21 @@ export function getAffection(version) {
 export function renderAffection(version) {
   return (params) => {
     const affection = getAffection(version)(params);
+
+    const onChange = (value) => {
+      if (params.row.issue_affects) {
+        params.row.issue_affects.filter(          
+          (affects) => affects.affect_version === version
+        )[0].affect_result = {"yes": "Yes" ,"no": "No" , "unknown":"UnKnown"}[value]; 
+      }
+    }
+
     return (
       <AffectionSelect
         id={params.row.issue.issue_id}
         version={version}
         affection={affection}
+        onChange={onChange}
       ></AffectionSelect>
     );
   };
